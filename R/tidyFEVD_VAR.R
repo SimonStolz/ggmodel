97d84a392910e198ea7e36f6ac2df56c4e5ad8dd
Stolz <simon.stolz@whu.edu>
2020-07-09 11:34:52 +0200

    Logo and functions

---
+++ R/tidyFEVD_VAR.R
@@ -0,0 +1,9 @@
+# Transforms "var" package's forecast error variance decomposition to tidy
+#
+# To input the data as ggplots, the data has to be put in to tidy
+# format first.
+#
+
+tidyFEVD_VAR <- function() {
+  print("# I will transform FEVD to tidy format")
+}
---
+++ R/tidyIRF_VAR.R
@@ -0,0 +1,16 @@
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+# Transforms "var" package's impulse response function (IRF) to tidy
+#
+# To input the data as ggplots, the data has to be put in to tidy
+# format first.
+#
+
+tidyIRF_VAR <- function() {
+  print("# I will transform IRF to tidy format")
+}
+
